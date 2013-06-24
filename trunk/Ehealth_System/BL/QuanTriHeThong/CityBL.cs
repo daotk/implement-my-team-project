04432fdf081395a633d29706fcc2874fc04aafe2
zintaen@gmail.com <zintaen@gmail.com@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-24 11:49:11 +0000

    Update classes CityBL, CityDA, CityDO Update City form Update function for button Add, Edit

---
+++ trunk/Ehealth_System/BL/QuanTriHeThong/CityBL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO.QuanTriHeThong;
+using DA.QuanTriHeThong;
+namespace BL.QuanTriHeThong
+{
+    public class CityBL
+    {
+        public List<CityDO> GetAllCity()
+        {
+            return CityDA.GetAllCities();
+        }
+        public static void add(String ID, String name, String desscription, bool status)
+        {
+
+            CityDA.add(ID, name, desscription, status);
+
+        }
+        public static void edit(String ID, String name, String desscription, bool status)
+        {
+
+            CityDA.edit(ID, name, desscription, status);
+
+        }
+
+
+
+
+
+    }
+}
---
+++ trunk/Ehealth_System/DA/QuanTriHeThong/CityDA.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data.Objects;
+using DO.QuanTriHeThong;
+namespace DA.QuanTriHeThong
+{
+    public class CityDA
+    {
+        public static List<CityDO> GetAllCities()
+        {
+            List<CityDO> ListCity = new List<CityDO>();
+            using (Entity.EHealthSystemEntities dk = new Entity.EHealthSystemEntities())
+            {
+                var query = from u in dk.City_Info select u;
+
+                foreach (var row in query)
+                {
+                    CityDO city = new CityDO();
+                    city._CITYID = row.CITYID;
+                    city._CITYNAME = row.CITYNAME;
+                    city._DESCRIPTIONCITY = row.DESCRIPTIONCITY;
+                    city._STATUSCITY = row.STATUSCITY;
+                    ListCity.Add(city);
+                }
+                return ListCity;
+            }
+        }
+        public static void add(String ID, String name, String desscription, bool status)
+        {
+
+            using (Entity.EHealthSystemEntities entity = new Entity.EHealthSystemEntities())
+            {
+                City_Info city = new City_Info();
+                city.CITYID = ID;
+                city.CITYNAME = name;
+                city.DESCRIPTIONCITY = desscription;
+                city.STATUSCITY = status;
+                entity.City_Info.Add(city);
+                entity.SaveChanges();
+
+            }
+        }
+        public static void edit(String ID, String name, String desscription, bool status)
+        {
+
+            using (Entity.EHealthSystemEntities entity = new Entity.EHealthSystemEntities())
+            {
+                var city = entity.City_Info.Single(p => p.CITYID == ID);
+
+                city.CITYID = ID;
+                city.CITYNAME = name;
+                city.DESCRIPTIONCITY = desscription;
+                city.STATUSCITY = status;
+
+                entity.SaveChanges();
+
+            }
+        }
+    }
+}
