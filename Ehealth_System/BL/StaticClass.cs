aba7624507fc4f93774035affc8ac3279668e854
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-25 05:42:00 +0000

    BL

--- Ehealth_System/BL/StaticClass.cs
+++ Ehealth_System/BL/StaticClass.cs
@@ -5,7 +5,9 @@ using System.Text;
 
 namespace BL
 {
-    public class Class1
+    public class StaticClass
     {
+        public static string UserName;
+        public static string GroupUser;
     }
 }
