e98498841288621e1f3e30b2a5337179b34cbe1f
huyngo233@gmail.com <huyngo233@gmail.com@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-25 03:17:18 +0000

    Add new department

---
+++ trunk/Ehealth_System/DO/QuanTriHeThong/DepartmentTypeDO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data.Objects;
+
+namespace DO.QuanTriHeThong
+{
+   
+    public class DepartmentTypeDO
+    {
+        public string _DEPARTMENTTYPEID { set; get; }
+        public string _DEPARTMENTNAME { set; get; }
+        public string _DEPARTMENTDESCRIPTION { set; get; }
+        public bool   _DEPARTMENTSTATUS { set; get; }
+          
+    }
+}
+
