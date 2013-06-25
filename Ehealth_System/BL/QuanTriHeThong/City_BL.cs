61c8a7c612f039e39391a1ed9c5d9365932fc45c
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-25 00:52:06 +0000

    update DA

---
+++ Ehealth_System/BL/QuanTriHeThong/City_BL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO.QuanTriHeThong;
+using DA.QuanTriHeThong;
+namespace BL.QuanTriHeThong
+{
+    public class City_BL
+    {
+        public List<City_DO> GetAllCity()
+        {
+            return City_DA.GetAllCities();
+        }
+        public static int add(String ID, String name, String desscription, bool status)
+        {
+
+            return City_DA.add(ID, name, desscription, status);
+
+        }
+        public static void edit(String ID, String name, String desscription, bool status)
+        {
+
+            City_DA.edit(ID, name, desscription, status);
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Ehealth_System/BL/QuanTriHeThong/UserGroup_BL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DA.QuanTriHeThong;
+using DO.QuanTriHeThong;
+
+namespace BL.QuanTriHeThong
+{
+    public class UserGroup_BL
+    {
+        public static List<UserGroup_DO> GetAllUsserGroup() {
+            return DA.QuanTriHeThong.UserGroup_DA.GetAllUserGroup();
+        }
+    }
+}
