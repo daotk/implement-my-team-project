748c67eb5c0fc08c18cc5dfa7526e3465a82ac7b
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-25 00:51:41 +0000

    update DO

---
+++ Ehealth_System/DO/QuanTriHeThong/City_DO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data.Objects;
+namespace DO.QuanTriHeThong
+{
+    public class City_DO
+    {
+        public string _CITYID { set; get; }
+        public string _CITYNAME { set; get; }
+        public string _DESCRIPTIONCITY { set; get; }
+        public bool _STATUSCITY { set; get; }
+
+    }
+
+}
