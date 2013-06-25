9becb61fddf42d9b8e9204f11f1c738ba5a03cee
huyngo233@gmail.com <huyngo233@gmail.com@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-25 03:17:54 +0000

    add new department

---
+++ Ehealth_System/BL/QuanTriHeThong/DepartmentTypeBL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DA.QuanTriHeThong;
+using DO.QuanTriHeThong;
+
+namespace BL.QuanTriHeThong
+{
+   public class DepartmentTypeBL
+    {
+        public List<DepartmentTypeDO> GetAllDepartment()
+        {
+            return DepartmentTypeDA.GetAllDepartments();
+        }
+        public static int add(String ID, String name, String desscription, bool status)
+        {
+
+            return DepartmentTypeDA.add(ID, name, desscription, status);
+
+        }
+        //public static void edit(String ID, String name, String desscription, bool status)
+        //{
+
+        //    DepartmentTypeDA.edit(ID, name, desscription, status);
+
+        //}
+    
+    }
+}
