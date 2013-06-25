5cda07b999b8cb9713836798e3ad8139b605722b
thienphuta1907 <thienphuta1907@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-25 02:59:23 +0000

    update create and edit user group

--- trunk/Ehealth_System/BL/QuanTriHeThong/UserGroup_BL.cs
+++ trunk/Ehealth_System/BL/QuanTriHeThong/UserGroup_BL.cs
@@ -12,5 +12,29 @@ namespace BL.QuanTriHeThong
         public static List<UserGroup_DO> GetAllUsserGroup() {
             return DA.QuanTriHeThong.UserGroup_DA.GetAllUserGroup();
         }
+        //Create user group
+        public static void CreateUserGroup(string tenviettats, string tennhoms, string motas
+            , bool trangthais)
+        {
+            DA.QuanTriHeThong.UserGroup_DA.CreateUserGroup(tenviettats, tennhoms, motas, trangthais);
+        }
+        // End create user group
+        
+        //Get info user group
+        public static List<UserGroup_DO> GetUserGroup(string tenviettat)
+        {
+            return DA.QuanTriHeThong.UserGroup_DA.GetUserGroup(tenviettat);
+        }
+        //End get info user group
+        //Edit Information of User Group
+
+        public static void EditUserGroup(string Tenviettat, string Tennhom, string Mota
+            , bool Trangthai)
+        {
+            DA.QuanTriHeThong.UserGroup_DA.EditUserGroup(Tenviettat, Tennhom, Mota, Trangthai);
+        }
+        //End Edit user group
+        
     }
+
 }
