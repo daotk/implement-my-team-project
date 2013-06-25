dfbc1d4e3443ffbb3d5b2c5b59f3d15b30bac792
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-25 00:52:54 +0000

    update DA

---
+++ Ehealth_System/DA/QuanTriHeThong/UserGroup_DA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO.QuanTriHeThong;
+namespace DA.QuanTriHeThong
+{
+    public class UserGroup_DA
+    {
+        public static List<UserGroup_DO> GetAllUserGroup() {
+            List<UserGroup_DO> dsusergroup = new List<UserGroup_DO>();
+            using (Entity.EHealthSystemEntities dk = new Entity.EHealthSystemEntities())
+            {
+                var query = from u in dk.UserType_Info select u;
+                foreach (var row in query) {
+                    UserGroup_DO us = new UserGroup_DO();
+                    us.tenviettat_ = row.USERTYPEID;
+                    us.tennhom_ = row.USERTYPENAME;
+                    us.trangthai = row.USERTYPESTATUS;
+                    us.mota_ = row.DESCRIPTION;
+
+                    dsusergroup.Add(us);
+                }
+            }
+            return dsusergroup;
+        }
+    }
+}
