22db07a523e3baf232a4b2db2271a205d4bdb3f6
huyngo233@gmail.com <huyngo233@gmail.com@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-26 02:01:00 +0000

    add and edit function

---
+++ Ehealth_System/BL/QuanTriHeThong/TypeDepartment_BL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO.QuanTriHeThong;
+using DA.QuanTriHeThong;
+
+namespace BL.QuanTriHeThong
+{
+   public class TypeDepartment_BL
+    {
+        public List<TypeDepartment_DO> GetAllDepartment()
+        {
+            return TypeDepartment_DA.GetAllDepartments();
+        }
+        public static int add(String ID, String name, String desscription, bool status)
+        {
+
+            return TypeDepartment_DA.add(ID, name, desscription, status);
+
+        }
+        public static void edit(String ID, String name, String desscription, bool status)
+        {
+
+            TypeDepartment_DA.edit(ID, name, desscription, status);
+
+        }
+    }
+}
---
+++ Ehealth_System/DA/QuanTriHeThong/TypeDepartment_DA.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO.QuanTriHeThong;
+using System.Data.Entity;
+using System.Data.Objects;
+using DO;
+
+
+namespace DA.QuanTriHeThong
+{
+    public class TypeDepartment_DA
+    {
+        public static List<TypeDepartment_DO> GetAllDepartments()
+        {
+            List<TypeDepartment_DO> ListDeparttype = new List<TypeDepartment_DO>();
+            using (Entity.EHealthSystemEntities dk = new Entity.EHealthSystemEntities())
+            {
+                var query = from u in dk.DepartmentType_Info select u;
+
+                foreach (var row in query)
+                {
+                    TypeDepartment_DO depart = new TypeDepartment_DO();
+                    depart._DEPARTMENTTYPEID = row.DEPARTMENTTYPEID;
+                    depart._DEPARTMENTNAME = row.DEPARTMENTTYPENAME;
+                    depart._DEPARTMENTDESCRIPTION = row.DEPARTMENTTYPEDESCRIPTION;
+                    depart._DEPARTMENTSTATUS = row.DEPARTMENTSTATUS;
+                    ListDeparttype.Add(depart);
+                }
+                return ListDeparttype;
+            }
+        }
+
+        public static int add(String ID, String name, String desscription, bool status)
+        {
+
+            using (Entity.EHealthSystemEntities entity = new Entity.EHealthSystemEntities())
+            {
+                Entity.DepartmentType_Info depart = new Entity.DepartmentType_Info();
+                depart.DEPARTMENTTYPEID = ID;
+                depart.DEPARTMENTTYPENAME = name;
+                depart.DEPARTMENTTYPEDESCRIPTION = desscription;
+                depart.DEPARTMENTSTATUS = status;
+                entity.DepartmentType_Info.AddObject(depart);
+                try
+                {
+                    int num = entity.SaveChanges();
+                    return num;
+                }
+                catch
+                {
+                    return -1;
+                }
+
+            }
+        }
+
+        public static void edit(String ID, String name, String desscription, bool status)
+        {
+
+            using (Entity.EHealthSystemEntities entity = new Entity.EHealthSystemEntities())
+            {
+                var depart = entity.DepartmentType_Info.Single(p => p.DEPARTMENTTYPEID == ID);
+
+                depart.DEPARTMENTTYPEID = ID;
+                depart.DEPARTMENTTYPENAME = name;
+                depart.DEPARTMENTTYPEDESCRIPTION = desscription;
+                depart.DEPARTMENTSTATUS = status;
+
+                entity.SaveChanges();
+
+            }
+        }
+    }
+}
