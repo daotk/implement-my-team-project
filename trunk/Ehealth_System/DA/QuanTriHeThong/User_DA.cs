8f54f7c371f30109ce260e68e4ff1e28d89aa5f5
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-26 16:19:13 +0000

    update user info

--- trunk/Ehealth_System/DA/QuanTriHeThong/User_DA.cs
+++ trunk/Ehealth_System/DA/QuanTriHeThong/User_DA.cs
@@ -12,12 +12,53 @@ namespace DA.QuanTriHeThong
         /// Lấy danh sách user tu database
         /// </summary>
         /// <returns></returns>
-        public static List<User_DO> GetAllUsers()
+        public static List<User_DO> GetAllUserInfo()
         {
             List<User_DO> dsUser = new List<User_DO>();
             using (DA.Entity.EHealthSystemEntities dk = new DA.Entity.EHealthSystemEntities())
             {
                 var query = from u in dk.User_Info select u;
+
+                foreach (var row in query)
+                {
+                    User_DO us = new User_DO();
+                    us._USERID = row.USERID;
+                    us._USERNAME = row.USERNAME;
+                    us._USERTYPEID = row.USERTYPEID;
+                    us._GROUPUSERNAME = row.UserType_Info.USERTYPENAME;
+                    us._EMAIL = row.EMAIL;
+                    us._ACCOUNT = row.ACCOUNT;
+                    us._PASSWORD = row.PASSWORD;
+                    us._AUTHO = row.UserType_Info.AUTHORUZATION;
+                    us._STATUS = row.STATUS;
+                    dsUser.Add(us);
+                }
+                return dsUser;
+            }
+        }//end 
+
+ 
+        public static void InsertUser(string IDUser, string HovaTen, string email,
+          string nhomnguoidung, string taikhoan, string password)
+        {
+            using (Entity.EHealthSystemEntities dk = new Entity.EHealthSystemEntities())
+            {
+                
+                
+
+            }
+        }
+
+        /// <summary>
+        /// Lấy danh sách user tu database theo ma
+        /// </summary>
+        /// <returns></returns>
+        public static List<User_DO> GetUserInfoFollowUserID(string userid)
+        {
+            List<User_DO> dsUser = new List<User_DO>();
+            using (DA.Entity.EHealthSystemEntities dk = new DA.Entity.EHealthSystemEntities())
+            {
+                var query = from u in dk.User_Info where u.USERID == userid select u;
 
                 foreach (var row in query)
                 {
@@ -34,42 +75,6 @@ namespace DA.QuanTriHeThong
                 return dsUser;
             }
         }//end 
-
-        /// <summary>
-        /// Lấy danh sách user tu database
-        /// </summary>
-        /// <returns></returns>
-        public static List<User1_DO> GetUserInfo()
-        {
-            List<User1_DO> dsUser = new List<User1_DO>();
-            using (DA.Entity.EHealthSystemEntities dk = new DA.Entity.EHealthSystemEntities())
-            {
-                var query = from u in dk.User_Info select u;
-
-                foreach (var row in query)
-                {
-                    User1_DO us = new User1_DO();
-                    us._USERID = row.USERID;
-                    us._USERNAME = row.USERNAME;
-                    us._GROUPUSERNAME = row.UserType_Info.USERTYPENAME;
-                    us._ACCOUNT = row.ACCOUNT;
-                    us._STATUS = row.STATUS;
-                    dsUser.Add(us);
-                }
-                return dsUser;
-            }
-        }//end 
-
-        public static void InsertUser(string IDUser, string HovaTen, string email,
-          string nhomnguoidung, string taikhoan, string password)
-        {
-            using (Entity.EHealthSystemEntities dk = new Entity.EHealthSystemEntities())
-            {
-                
-                
-
-            }
-        }
 
     }
 }
