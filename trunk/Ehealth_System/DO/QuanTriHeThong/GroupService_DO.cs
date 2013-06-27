b94842db8454b23f93d705a6d23f18b4043366e9
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-27 13:38:27 +0000

    update Group service

---
+++ trunk/Ehealth_System/DO/QuanTriHeThong/GroupService_DO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO.QuanTriHeThong
+{
+    public class GroupService_DO
+    {
+        public string _SERVICEGROUPID { set; get; }
+        public string _SERVICEGROUPNAME { set; get; }
+        public string _SERVICEBROUPDESCRIPTION { set; get; }
+        public bool _SERVICEGROUPSTATUS { set; get; }
+    }
+}
