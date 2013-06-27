3e8d579a0b2d035141afc98dc62c469903a0da13
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-27 13:41:29 +0000

    update Group service

---
+++ trunk/Ehealth_System/BL/QuanTriHeThong/GroupService_BL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DA.QuanTriHeThong;
+using DO.QuanTriHeThong;
+namespace BL.QuanTriHeThong
+{
+    public class GroupService_BL
+    {
+        public static List<GroupService_DO> GetGroupService()
+        {
+            return DA.QuanTriHeThong.GroupService_DA.GetGroupService();
+        }
+        public static void CreateService(string serviceid, string servicename, string servicedescription, bool trangthais)
+        {
+            DA.QuanTriHeThong.GroupService_DA.CreateService(serviceid, servicename, servicedescription, trangthais);
+        }
+        public static void EditService(string serviceid, string servicename, string servicedescription, bool trangthais)
+        {
+            DA.QuanTriHeThong.GroupService_DA.EditService(serviceid, servicename, servicedescription, trangthais);
+        }
+        public static List<GroupService_DO> Get_GroupService(string tenviettats)
+        {
+            return DA.QuanTriHeThong.GroupService_DA.Get_GroupService(tenviettats);
+        }
+    }
+}
