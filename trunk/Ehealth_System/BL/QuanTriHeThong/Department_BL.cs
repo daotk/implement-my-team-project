e1fbe2770eaa78f64dc22751114281958643f009
huyngo233@gmail.com <huyngo233@gmail.com@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-27 02:33:11 +0000

    add function "create"

---
+++ trunk/Ehealth_System/BL/QuanTriHeThong/Department_BL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DA.QuanTriHeThong;
+using DO.QuanTriHeThong;
+using DA;
+
+namespace BL.QuanTriHeThong
+{
+   public class Department_BL
+    {
+       public List<Department_DO> GetAllDepart() 
+       {
+           return Department_DA.GetAllDeparts();
+       }
+
+       public static int add(String ID, String name, String DepartmentID, String desscription, bool status)
+       {
+           return Department_DA.add(ID, name, DepartmentID, desscription, status);
+       }
+
+
+       public static void edit(String ID, String name, String CityID, String desscription, bool status)
+       {
+           District_DA.edit(ID, name, CityID, desscription, status);
+       }
+
+
+       public static List<District_DO> SearchDistrict(String name)
+       {
+           return District_DA.SearchDistrict(name);
+       }
+
+
+       public static List<District_DO> SearchDistrictByBoth(String name, String city)
+       {
+           return District_DA.SearchDistrictByBoth(name, city);
+       }
+
+
+       public static List<District_DO> SearchDistrictByCity(String city)
+       {
+           return District_DA.SearchDistrictByCity(city);
+       }
+
+
+
+    }
+}
