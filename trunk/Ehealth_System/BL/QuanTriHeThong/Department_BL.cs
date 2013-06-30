101d03dcddff4c5e8427675c2015036de0e614ea
huyngo233@gmail.com <huyngo233@gmail.com@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-06-30 05:00:20 +0000

    add comment

--- trunk/Ehealth_System/BL/QuanTriHeThong/Department_BL.cs
+++ trunk/Ehealth_System/BL/QuanTriHeThong/Department_BL.cs
@@ -15,30 +15,62 @@ namespace BL.QuanTriHeThong
            return Department_DA.GetAllDeparts();
        }
 
+
+       /// <summary>
+       /// thêm phòng ban mới
+       /// </summary>
+       /// <param name="ID"></param>
+       /// <param name="name"></param>
+       /// <param name="DepartmentID"></param>
+       /// <param name="desscription"></param>
+       /// <param name="status"></param>
+       /// <returns></returns>
        public static int add(String ID, String name, String DepartmentID, String desscription, bool status)
        {
            return Department_DA.add(ID, name, DepartmentID, desscription, status);
        }
 
-
+       /// <summary>
+       /// chỉnh sửa phòng ban
+       /// </summary>
+       /// <param name="ID"></param>
+       /// <param name="name"></param>
+       /// <param name="DepartmentID"></param>
+       /// <param name="desscription"></param>
+       /// <param name="status"></param>
+       /// <returns></returns>
        public static int edit(String ID, String name, String DepartmentID, String desscription, bool status)
        {
            return Department_DA.edit(ID, name, DepartmentID, desscription, status);
        }
 
-
+       /// <summary>
+       /// tìm kiếm phòng ban theo tên
+       /// </summary>
+       /// <param name="name"></param>
+       /// <returns></returns>
        public static List<Department_DO> SearchDepart(String name)
        {
            return Department_DA.SearchDepartment(name);
        }
 
 
+       /// <summary>
+       /// tìm kiếm phòng ban theo từ khóa và theo tên loại phòng ban
+       /// </summary>
+       /// <param name="name"></param>
+       /// <param name="city"></param>
+       /// <returns></returns>
        public static List<Department_DO> SearchDepartByBoth(String name, String city)
        {
            return Department_DA.SearchDepartByBoth(name, city);
        }
 
-
+       /// <summary>
+       /// tìm kiếm theo loại phòng ban
+       /// </summary>
+       /// <param name="city"></param>
+       /// <returns></returns>
        public static List<Department_DO> SearchDistrByDeparttype(String city)
        {
            return Department_DA.SearchDistrByDeparttype(city);
@@ -47,4 +79,4 @@ namespace BL.QuanTriHeThong
 
 
     }
-}
+}//end class
