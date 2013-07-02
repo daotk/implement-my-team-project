be3daa08c81d9620b434b13d2d9485a835ea5a0d
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-02 03:35:29 +0000

    add Desk_DA

---
+++ trunk/Ehealth_System/DA/ThuNgan/Desk_DA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DA.ThuNgan
+{
+    public class Desk_DA
+    {
+        public static List<DO.ThuNgan.Desk_DO> GetAllDesk()
+        {
+            List<DO.ThuNgan.Desk_DO> ListDesk = new List<DO.ThuNgan.Desk_DO>();
+            using (Entity.EHealthSystemEntities dk = new Entity.EHealthSystemEntities())
+            {
+                var query = from u in dk.DeskCashiers  select u;
+                foreach (var row in query)
+                {
+                    DO.ThuNgan.Desk_DO desk = new DO.ThuNgan.Desk_DO();
+                    desk._DESKID = row.DESKID;
+                    desk._DESKNAME = row.DESKNAME;
+                    desk._DEPARTMENTID = row.DEPARTMENTID;
+                    desk._DEPARTMENTNAME = row.Department_Info.DEPARTMENTNAME;
+                    desk._DESKSTATUS = row.DESKSTATUS;
+                    ListDesk.Add(desk);
+                }
+                return ListDesk;
+            }
+        }
+    }
+}
