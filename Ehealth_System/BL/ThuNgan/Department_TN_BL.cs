25c8bba098d8830e25efb32d2b47984c39aaf5a5
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-02 03:35:55 +0000

    Add Desk_BL

--- Ehealth_System/BL/ThuNgan/Department_TN_BL.cs
+++ Ehealth_System/BL/ThuNgan/Department_TN_BL.cs
@@ -6,75 +6,15 @@ using DA.QuanTriHeThong;
 using DO.QuanTriHeThong;
 using DA;
 
-namespace BL.QuanTriHeThong
+namespace BL.ThuNgan
 {
    public class Department_TN_BL
     {
-       public List<Department_DO> GetAllDepart() 
+       public static List<DO.ThuNgan.Department_TN_DO> GetAllDepart() 
        {
-           return Department_DA.GetAllDeparts();
+           return DA.ThuNgan.Department_TN_DA.GetAllDepart();
        }
 
-
-       /// <summary>
-       /// thêm phòng ban mới
-       /// </summary>
-       /// <param name="ID"></param>
-       /// <param name="name"></param>
-       /// <param name="DepartmentID"></param>
-       /// <param name="desscription"></param>
-       /// <param name="status"></param>
-       /// <returns></returns>
-       public static int add(String ID, String name, String DepartmentID, String desscription, bool status)
-       {
-           return Department_DA.add(ID, name, DepartmentID, desscription, status);
-       }
-
-       /// <summary>
-       /// chỉnh sửa phòng ban
-       /// </summary>
-       /// <param name="ID"></param>
-       /// <param name="name"></param>
-       /// <param name="DepartmentID"></param>
-       /// <param name="desscription"></param>
-       /// <param name="status"></param>
-       /// <returns></returns>
-       public static int edit(String ID, String name, String DepartmentID, String desscription, bool status)
-       {
-           return Department_DA.edit(ID, name, DepartmentID, desscription, status);
-       }
-
-       /// <summary>
-       /// tìm kiếm phòng ban theo tên
-       /// </summary>
-       /// <param name="name"></param>
-       /// <returns></returns>
-       public static List<Department_DO> SearchDepart(String name)
-       {
-           return Department_DA.SearchDepartment(name);
-       }
-
-
-       /// <summary>
-       /// tìm kiếm phòng ban theo từ khóa và theo tên loại phòng ban
-       /// </summary>
-       /// <param name="name"></param>
-       /// <param name="city"></param>
-       /// <returns></returns>
-       public static List<Department_DO> SearchDepartByBoth(String name, String city)
-       {
-           return Department_DA.SearchDepartByBoth(name, city);
-       }
-
-       /// <summary>
-       /// tìm kiếm theo loại phòng ban
-       /// </summary>
-       /// <param name="city"></param>
-       /// <returns></returns>
-       public static List<Department_DO> SearchDistrByDeparttype(String city)
-       {
-           return Department_DA.SearchDistrByDeparttype(city);
-       }
 
 
 
