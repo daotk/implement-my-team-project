6e09ccb4b21f4dfe2a25eea211ae899ce23756aa
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-02 03:35:55 +0000

    Add Desk_BL

---
+++ trunk/Ehealth_System/BL/ThuNgan/Desk_BL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DA;
+using DO;
+
+namespace BL.ThuNgan
+{
+    public class Desk_BL
+    {
+        public static List<DO.ThuNgan.Desk_DO> GetAllDesk()
+        {
+            return DA.ThuNgan.Desk_DA.GetAllDesk();
+        }
+    }
+}
