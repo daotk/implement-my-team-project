49bc80f0d83cb5fd2c0f9f33b3e05247c2b7d790
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-02 03:34:33 +0000

    Add Desk_DO

---
+++ trunk/Ehealth_System/DO/ThuNgan/Desk_DO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO.ThuNgan
+{
+    public class Desk_DO
+    {
+        public string _DESKID{set;get;}
+        public string _DEPARTMENTID{set;get;}
+        public string _DEPARTMENTNAME { set; get; }
+        public string _DESKNAME{set;get;}
+        public bool _DESKSTATUS { set; get; }
+    }
+}
