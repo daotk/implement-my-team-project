6fbf1ff6fb662661af3693182f0c1e9f5d03afb1
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-02 02:02:23 +0000

    update Desk_BL

---
+++ trunk/Ehealth_System/BL/QuanTriHeThong/Desk_BL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DA;
+using DO;
+
+namespace BL.QuanTriHeThong
+{
+    public class Desk_BL
+    {
+        public static List<DO.QuanTriHeThong.Desk_DO> GetDesk(string id)
+        {
+            return DA.QuanTriHeThong.Desk_DA.GetDesk(id);
+        }
+
+    }
+}
