6e3bea30659d2dddc6908b6355414db363b2e46c
daokhau1 <daokhau1@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-02 02:05:04 +0000

    update DA

---
+++ trunk/Ehealth_System/DA/QuanTriHeThong/Desk_DA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+
+namespace DA.QuanTriHeThong
+{
+    public class Desk_DA
+    {
+        /// <summary>
+        /// Lấy danh sách user tu database
+        /// </summary>
+        /// <returns></returns>
+        public static List<DO.QuanTriHeThong.Desk_DO> GetDesk(string departmentid)
+        {
+            List<DO.QuanTriHeThong.Desk_DO> dsUser = new List<DO.QuanTriHeThong.Desk_DO>();
+            using (DA.Entity.EHealthSystemEntities dk = new DA.Entity.EHealthSystemEntities())
+            {
+                var query = from u in dk.DeskCashiers where u.DEPARTMENTID == departmentid select u;
+
+                foreach (var row in query)
+                {
+                    DO.QuanTriHeThong.Desk_DO us = new DO.QuanTriHeThong.Desk_DO();
+                    us._DESKID = row.DESKID;
+                    us._DEPARTMENTID = row.DEPARTMENTID;
+                    us._DESKNAME = row.DESKNAME;
+                    us._DESKSTATUS = row.DESKSTATUS;
+                    dsUser.Add(us);
+                }
+                return dsUser;
+            }
+        }//end 
+    }
+}
