18ddab35fc5868f7353ac0ce56b32128ed87d2e2
thienphuta1907 <thienphuta1907@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-05 04:13:41 +0000

    update Cashier

--- trunk/Ehealth_System/DO/ThuNgan/TypistDO.cs
+++ trunk/Ehealth_System/DO/ThuNgan/TypistDO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace DO.ThuNgan
+namespace DO.Thu_Ngan
 {
     public class BillDO {
         public string billid_ { get; set; }
