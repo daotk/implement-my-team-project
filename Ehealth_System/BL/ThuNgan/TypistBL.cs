b9b7ec2cc87bbb0e8cf0a18c23a8ab5cdfd52a30
thienphuta1907 <thienphuta1907@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-05 04:14:21 +0000

    update Cashier

--- Ehealth_System/BL/ThuNgan/TypistBL.cs
+++ Ehealth_System/BL/ThuNgan/TypistBL.cs
@@ -2,63 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using DA.ThuNgan;
-using DO.ThuNgan;
-namespace BL.ThuNgan
+using DA.Thu_Ngan;
+using DO.Thu_Ngan;
+namespace BL.Thu_Ngan
 {
     public class TypistBL
     {
         public static List<TypistDO> LoadDSServiceType()
         {
-            return DA.ThuNgan.TypistDA.LoadDSServiceType();
+            return DA.Thu_Ngan.TypistDA.LoadDSServiceType();
         }
         public static List<TypistDO> LoadDSService(string maloaidichvu)
         {
-            return DA.ThuNgan.TypistDA.LoadDSService(maloaidichvu);
+            return DA.Thu_Ngan.TypistDA.LoadDSService(maloaidichvu);
         }
         public static List<PatientDO> LoadPatientInfo(string mabenhnhan)
         {
-            return DA.ThuNgan.TypistDA.LoadPatientInfo(mabenhnhan);
+            return DA.Thu_Ngan.TypistDA.LoadPatientInfo(mabenhnhan);
         }
         public static List<CostDO> LoadServiceCost(string tendichvu)
         {
-            return DA.ThuNgan.TypistDA.LoadServiceCost(tendichvu);
+            return DA.Thu_Ngan.TypistDA.LoadServiceCost(tendichvu);
         }
         public static void CreateBill(string madichvu, string mabenhnhan, string manguoidung
            , string maban, string chiphihoadon, bool trangthaihoadon, string servicegroupid)
         {
 
-             DA.ThuNgan.TypistDA.CreateBill(madichvu, mabenhnhan, manguoidung
+             DA.Thu_Ngan.TypistDA.CreateBill(madichvu, mabenhnhan, manguoidung
            , maban, chiphihoadon, trangthaihoadon, servicegroupid); 
         }
 
         public static void CreateDetailBill( string madichvu, string chiphidichvu, string mahoadon)
         {
 
-            DA.ThuNgan.TypistDA.CreateDetailBill( madichvu, chiphidichvu, mahoadon);
+            DA.Thu_Ngan.TypistDA.CreateDetailBill( madichvu, chiphidichvu, mahoadon);
         
         }
         public static string LoadIDLoaidichvu(string tenloaidichvu, string maloaidichvu)
         {
-            return DA.ThuNgan.TypistDA.LoadIDLoaidichvu(tenloaidichvu, maloaidichvu);
+            return DA.Thu_Ngan.TypistDA.LoadIDLoaidichvu(tenloaidichvu, maloaidichvu);
         }
         public static string LoadIDdichvu(string tendichvu, string madichvu)
         {
-            return DA.ThuNgan.TypistDA.LoadIDdichvu(tendichvu, madichvu);
+            return DA.Thu_Ngan.TypistDA.LoadIDdichvu(tendichvu, madichvu);
         }
 
         public static string LoadIDBill(string tenloaidichvu, string mabill)
         {
-            return DA.ThuNgan.TypistDA.LoadIDBill(tenloaidichvu, mabill);
+            return DA.Thu_Ngan.TypistDA.LoadIDBill(tenloaidichvu, mabill);
         }
         public static List<PatientDO> LoadDSMaBenhNhan()
         {
-            return DA.ThuNgan.TypistDA.LoadDSMaBenhNhan();
+            return DA.Thu_Ngan.TypistDA.LoadDSMaBenhNhan();
         }
 
         public static void capnhatongtien(string maloaidichvu, string tongtien)
         {
-            DA.ThuNgan.TypistDA.capnhatongtien(maloaidichvu, tongtien);
+            DA.Thu_Ngan.TypistDA.capnhatongtien(maloaidichvu, tongtien);
         }
     }
 }
