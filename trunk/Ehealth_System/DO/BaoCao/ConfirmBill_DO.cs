efc1270319c9a66ce71f827aa7577490c9b25429
huyngo233@gmail.com <huyngo233@gmail.com@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-07 07:44:03 +0000

    confirm bill_DO

---
+++ trunk/Ehealth_System/DO/BaoCao/ConfirmBill_DO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO.BaoCao
+{
+    public class ConfirmBill_DO
+    {
+        public string _BILLID { set; get; }
+        public string _PATIENTID { set; get; }
+        public string _USERID { set; get; }
+        public string _DESKID { set; get; }
+        public string _SERVICEGROUPNAME { set; get; }
+        public DateTime _BILLDATE { set; get; }
+        public string _BILLCOST { set; get; }
+        public bool _BILLSTATUS { set; get; }
+    }//end class
+}//end namespace
