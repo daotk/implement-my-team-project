57f5e5fa703db3a0b0cc39c06682d4a33bc59338
huyngo233@gmail.com <huyngo233@gmail.com@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-07 07:44:46 +0000

    ConfirmBill_BL

---
+++ Ehealth_System/BL/BaoCao/ConfirmBill_BL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO.BaoCao;
+using DA.BaoCao;
+
+namespace BL.BaoCao
+{
+   public class ConfirmBill_BL
+    {
+       public List<ConfirmBill_DO> GetAllBill()
+       {
+           return ConfirmBill_DA.GetAllBills();
+       }
+    }
+}
