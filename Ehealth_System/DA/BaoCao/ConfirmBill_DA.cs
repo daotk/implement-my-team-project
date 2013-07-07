6688464ceb42b808432093a46af5479973dd9fb5
huyngo233@gmail.com <huyngo233@gmail.com@57cb0d0d-b47c-09c8-d120-ab16406c9cd5>
2013-07-07 07:44:23 +0000

    Confirmbill_DA

---
+++ Ehealth_System/DA/BaoCao/ConfirmBill_DA.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BL.BaoCao;
+using DO.BaoCao;
+
+namespace DA.BaoCao
+{
+    public class ConfirmBill_DA
+    {
+        public static List<ConfirmBill_DO> GetAllBills()
+        {
+            //initialize constructor to get data from Entity model and assign them to grid view
+            List<ConfirmBill_DO> ListBill = new List<ConfirmBill_DO>();
+            using (Entity.EHealthSystemEntities dk = new Entity.EHealthSystemEntities())
+            {
+                var query = from u in dk.Bill_Info select u;
+                foreach (var row in query)
+                {
+                    ConfirmBill_DO bill = new ConfirmBill_DO();
+                    bill._BILLID = row.BILLID;
+                    bill._PATIENTID = row.PATIENTID;
+                    bill._USERID = row.USERID;
+                    bill._DESKID = row.DESKID;
+                    bill._SERVICEGROUPNAME = row.SERVICEGROUPNAME;
+                    bill._BILLDATE = row.BILLDATE;
+                    bill._BILLCOST = row.BILLCOST;
+                    bill._BILLSTATUS = row.BILLSTATUS;
+                    ListBill.Add(bill);
+                }
+                //return a list of bill
+                return ListBill;
+            }
+        }
+    }
+}
